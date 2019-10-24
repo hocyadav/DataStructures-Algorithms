69993a80c2bbbeaa1a4989c8d61ea03522eb5afa
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-10-24 12:13:08 +0530

    Stack implementation using 1 array with fixed size

--- DataStructures-Algorithms-BASIC/src/main/java/com/datastructure/Stack_ArrayImplementation.java
+++ DataStructures-Algorithms-BASIC/src/main/java/com/datastructure/Stack_ArrayImplementation.java
@@ -1,10 +1,48 @@
 package com.datastructure;
 
 public class Stack_ArrayImplementation {
+	//declare global variable
+	static int stackSize	= 11;
+	static int[] stack 	= new int[stackSize];
+	static int top 		= -1;
 
+	static void push(int x){//increment TOP and add && check overflow condition
+		if(top == stackSize-1) {
+			System.out.println("STACK : overflow - no space left");
+			return;
+		}
+		stack[++top] = x;
+	}
+	
+	static void pop(){//here pop implementaion : only decrement top
+		top--;
+	}
+	int top(){//get top element && return only if not empty
+		return stack[top];
+	}
+	static void print(){
+		System.out.print("STACK : ");
+		for(int i=0; i<=top; i++) {
+			System.out.print(" "+stack[i]);
+		}
+		System.out.println("");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		push(1); print();
+		push(3); print();
+		push(6); print();
+		pop(); print();
+		push(11); print();
+		push(14); print();
+		push(155); print();
+		push(13); print();
+		push(17); print();
+		push(123); print();
+		push(19); print();
+		push(144); print();
+		push(15); print();//overflow - only work we we change value stackSize to 11
+		
 	}
 
 }
