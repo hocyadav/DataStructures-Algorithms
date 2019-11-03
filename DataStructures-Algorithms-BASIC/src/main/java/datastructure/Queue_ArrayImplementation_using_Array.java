7e99581be6923d331ffeb39437f5233d700b37ed
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-03 09:58:36 +0530

    Queue_ArrayImplementation_using_Array

---
+++ DataStructures-Algorithms-BASIC/src/main/java/datastructure/Queue_ArrayImplementation_using_Array.java
@@ -0,0 +1,59 @@
+package datastructure;
+
+//data structure - array
+//use of ds
+class Queue{
+	//know something
+	int front, rear;
+	int size;
+	int[] queue;
+	
+	//does something
+	Queue(int s) {
+		front = 0; rear = 0;
+		size = s;
+		queue = new int[size];
+	}
+	
+	//insert
+	void enQueue(int v) {//rear++ & add (in stack top++ & add)
+		if(rear == size) {
+			System.out.println("Overflow");
+			return;
+		}
+		queue[rear++] = v;
+	}
+	
+	void deQueue() {
+		if(front == rear) {
+			System.out.println("Underflow - nothing to delete");
+			return;
+		}else {
+			for(int i=0; i<rear-1;i++) {//move 1 step left
+				queue[i] = queue[i+1];
+			}
+			--rear;//no need to do queu[rear] = 0
+		}
+	}
+	
+	void print() {
+		System.out.print("Queue : ");
+		for(int i=0;i<rear;i++) {//no equal to real becaue after enqueue rear in incrementing
+			System.out.print(queue[i]+" ");
+		}
+		System.out.println("");
+	}
+	
+}
+
+public class Queue_ArrayImplementation_using_Array {
+	public static void main(String[] arg) {
+		Queue obj = new Queue(4);
+		obj.deQueue();obj.print();
+		obj.enQueue(1);obj.print();
+		obj.enQueue(2);obj.print();
+		obj.deQueue();obj.print();
+		obj.deQueue();obj.print();
+		obj.deQueue();obj.print();
+	}
+}
